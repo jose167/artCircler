5ef2974ae32a98de0abb2f8fde760d5e6e5d2cc4
josemalave167 <josemalave167@gmail.com>
2016-06-14 21:53:52 +0100

    Add project files.

---
+++ ArtCircler/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ArtCircler.MvcApplication" Language="C#" %>
---
+++ ArtCircler/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArtCircler.Startup))]
+namespace ArtCircler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
