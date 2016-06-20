3fb75b0db04e34e16d2055185891af542e023d3c
josemalave167 <josemalave167@gmail.com>
2016-06-20 23:36:12 +0100

    form event

---
+++ ArtCircler/Controllers/EventsController.cs
@@ -0,0 +1,27 @@
+﻿using ArtCircler.Models;
+using ArtCircler.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ArtCircler.Controllers
+{
+    public class EventsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public EventsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new EventFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ ArtCircler/ViewModels/EventFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ArtCircler.Models;
+using System.Collections.Generic;
+
+namespace ArtCircler.ViewModels
+{
+    public class EventFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
