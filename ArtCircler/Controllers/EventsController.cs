7987c54a561e7db01bd620e9ec43e8f4c7962123
josemalave167 <josemalave167@gmail.com>
2016-06-29 12:56:55 +0100

    improving vulnerability (csrf)

--- ArtCircler/Controllers/EventsController.cs
+++ ArtCircler/Controllers/EventsController.cs
@@ -28,6 +28,7 @@ namespace ArtCircler.Controllers
 
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(EventFormViewModel viewModel)
         {
             if (!ModelState.IsValid)
