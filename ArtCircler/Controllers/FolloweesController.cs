d16028c3a85e5518673e944ee6596f5071ef316c
josemalave167 <josemalave167@gmail.com>
2016-07-06 16:39:02 +0100

    LIST FOLLOWERS

---
+++ ArtCircler/Controllers/FolloweesController.cs
@@ -0,0 +1,34 @@
+﻿using ArtCircler.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArtCircler.Controllers
+{
+    public class FolloweesController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public FolloweesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Followees
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var artists = _context.Followings
+                .Where(f => f.FollowerId == userId)
+                .Select(f => f.Followee)
+                .ToList();
+
+
+            return View(artists);
+        }
+    }
+}
